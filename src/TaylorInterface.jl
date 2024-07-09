8f26644ae04e45eff88b5004faf63efba0bbf5ea
Alseïdon <alexandre.prieur@obspm.fr>
2024-07-09 16:54:24 +0200

    Add func to regenerate models

--- src/TaylorInterface.jl
+++ src/TaylorInterface.jl
@@ -9,4 +9,21 @@ export get_handler
 export open_lib, close_lib
 export is_open, check_open
 
+function __regenerate_model_dirs(check=true)
+    eqfiles = readdir(joinpath(get_models_dir(), "eqs"), join=true)
+    modelnames = map(i->split(i, ['.', '/'])[2], eqfiles)
+    gens = map(
+        (name, eqfile)->TaylorGenerator(name, eqfile, "."),
+        modelnames, eqfiles
+    )
+    map(generate_dir, gens)
+    if check
+        handlers = map(get_handler, gens)
+        map(open_lib, handlers)
+        map(close_lib, handlers)
+    end
+    return
+end
+
+
 end
