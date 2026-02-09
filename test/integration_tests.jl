faf0aa87af7f86b1603172cef68265e6bf1bf1be
Alseïdon <alexandre.prieur@obspm.fr>
2026-02-09 19:09:09 +0100

    Add test for extern vars

--- test/integration_tests.jl
+++ test/integration_tests.jl
@@ -17,7 +17,7 @@
 end
 
 @testset "Integration test -- jet" begin
-    #= NO JET =#
+    #= JET =#
     gen = TaylorGenerator("sincos_jet", "sincos_jet.eqs", ".")
     generate_dir(gen, true)
     han = get_handler(gen, true)
@@ -35,3 +35,25 @@ end
     close_lib(han)
     TaylorInterface.clear_dir(gen)
 end
+
+# @testset "Integration test -- extern" begin
+    gen = TaylorGenerator("sincos_extern", "sincos_extern.eqs", ".")
+    generate_dir(gen, true)
+    han = get_handler(gen, true)
+    
+    x = [1., 0.]
+    df = zeros(4)
+    k = 2.
+    set_extern_var(han, "k", k)
+
+    
+    ts = LinRange(0., 2π, 101)
+    ys = map(t->flow!(han, x, t, df), ts)
+    y1 = map(i->i[1], ys)
+    y2 = map(i->i[2], ys)
+    
+    @test y1 ≈ cos.(k * ts)
+    @test y2 ≈ -sin.(k * ts)
+    close_lib(han)
+    TaylorInterface.clear_dir(gen)
+# end
